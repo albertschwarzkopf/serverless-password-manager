1835b33de31238e0f5bab424a998a7f65adf5486
Ubuntu <ubuntu@ip-172-31-21-2.eu-west-1.compute.internal>
2019-02-11 13:37:35 +0000

    last commit today

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,13 +4,13 @@ export default {
 //    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
 //  },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "eu-west-1",
+    URL: "https://z8f27xnzp2.execute-api.eu-west-1.amazonaws.com/dev/password"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_BG7qtb542",
+    APP_CLIENT_ID: "51a3foj807svijkoia2aqitmjl",
+    IDENTITY_POOL_ID: "eu-west-1:b020d3af-693e-49d3-967c-9b65e4bd7fab"
   }
 };
