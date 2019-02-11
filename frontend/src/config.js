dab86025f639ee4a1f606f693de345183226632c
Ubuntu <ubuntu@ip-172-31-21-2.eu-west-1.compute.internal>
2019-02-11 14:52:08 +0000

    adds Logout

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,12 +5,12 @@ export default {
 //  },
   apiGateway: {
     REGION: "eu-west-1",
-    URL: "https://z8f27xnzp2.execute-api.eu-west-1.amazonaws.com/dev/password"
+    URL: "https://v7wi3gvsdk.execute-api.eu-west-1.amazonaws.com/dev/password"
   },
   cognito: {
     REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_BG7qtb542",
-    APP_CLIENT_ID: "51a3foj807svijkoia2aqitmjl",
-    IDENTITY_POOL_ID: "eu-west-1:b020d3af-693e-49d3-967c-9b65e4bd7fab"
+    USER_POOL_ID: "eu-west-1_7JPZ6c1Y1",
+    APP_CLIENT_ID: "5eo6jr2dj9t43tj8sd8881m5bo",
+    IDENTITY_POOL_ID: "eu-west-1:f7390fb6-ac62-4359-9f49-59554389a2dd"
   }
 };
